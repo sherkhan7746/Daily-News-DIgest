dc9fd10a30d05a48c38558dc9bafa7ec51ec0a0e
sherkhan <sherkhan1167746@gmail.com>
2024-01-19 14:29:04 +0500

    add email functiona;ity

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import os
-
+from send_email import send_email
 
 api_key = os.getenv("APIKEY")
+
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-12-19&sortBy=publishedAt&apiKey=" + api_key
 # Make requests
 request = requests.get(url=url)
@@ -10,7 +11,13 @@ request = requests.get(url=url)
 # Get a dictionary with data
 content = request.json()
 
+body = ""
 # get articles with description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + "\n"
+
+send_email(body)
+
+
+
